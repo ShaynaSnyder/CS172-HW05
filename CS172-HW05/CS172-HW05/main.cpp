9a6d9947fc5990001da8965522e5260534d8704e
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-11-01 12:39:07 -0700

    Initial commit

---
+++ CS172-HW05/CS172-HW05/main.cpp
@@ -0,0 +1,17 @@
+//Shayna Snyder
+//CS172-1
+//1 November, 2016
+//HW05
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
